18eaa6e82a6f3e22851275f63b16e7aaaab4f45b
Sigurdmazanti <sigmn@live.dk>
2021-05-21 14:03:37 +0200

    ok

--- javascript/instafeed.js
+++ javascript/instafeed.js
@@ -32,7 +32,7 @@
       sort: null,
       success: null,
       target: "instafeed",
-      template: '<a href="{{link}}"><div class="img-featured-container"><div class="img-backdrop"></div><div class="description-container"><p class="caption">{{caption}}</p><span class="likes"><i class="icon ion-heart"></i> {{likes}}</span><span class="comments"><i class="icon ion-chatbubble"></i> {{comments}}</span></div><img src="{{image}}" class="img-responsive"></div></a></div>',
+      template: '<a href="{{link}}"><div class="img-featured-container"><div class="img-backdrop"></div><div class="description-container"><p class="caption">{{caption}}</p><span class="likes"><i class="fas fa-heart"></i> {{likes}}</span><span class="comments"><i class="fas fa-comment"></i> {{comments}}</span></div><img src="{{image}}" class="img-responsive"></div></a></div>',
       templateBoundaries: [ "{{", "}}" ],
       transform: null
     };
